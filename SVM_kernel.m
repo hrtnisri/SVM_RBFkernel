cabd6b6b06f48b845df2e646f7e528feaa80168c
untitledoor <sri.hartini@sci.ui.ac.id>
2019-06-29 01:20:38 +0700

    Add files via upload

---
+++ SVM_kernel.m
@@ -0,0 +1,33 @@
+clear all
+clc
+
+% Input data Schizophrenia
+cldata=xlsread('Schizophrenia.xlsx');
+[nrow,ncol]=size(cldata);
+
+% Split data into 70% train and 30% test
+rng('default'); 
+cv = cvpartition(size(cldata,1), 'HoldOut', 0.3);
+idx = cv.test;
+dataTrain = cldata(~idx,:);
+dataTest = cldata(idx,:);
+
+% Training data
+X = dataTrain(:,1:ncol-1);
+Y = dataTrain(:,ncol);
+tic
+SVMModel = fitcsvm(X,Y,'KernelFunction','rbf','KernelScale', 1/(sqrt(0.1)),'BoxConstraint',Inf);
+CVSVMModel = crossval(SVMModel);
+
+% Testing Data
+XTest = dataTest(:,1:ncol-1);
+YTest = dataTest(:,ncol);
+label = predict(SVMModel,XTest);
+
+% Counting the running time
+timeElapsed=toc;
+fprintf('Running Time: %i\n', timeElapsed);
+
+% Check the accuracy
+cp = classperf(YTest, label);
+fprintf('Accuracy: %i\n', cp.CorrectRate*100);
